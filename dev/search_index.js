840876e038bdd791bdd6a61514154c42ca5730d9
Documenter.jl <documenter@juliadocs.github.io>
2024-09-29 13:55:54 +0000

    build based on 16d7f7d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GamePieces","category":"page"},{"location":"#GamePieces","page":"Home","title":"GamePieces","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GamePieces.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GamePieces]","category":"page"}]
+}
